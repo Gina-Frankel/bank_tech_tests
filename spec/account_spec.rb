9a33cca3633542d18f41fdfb6b56b586c5a3b8fc
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-18 23:41:11 +0100

    Passing : .Account - .Account  #deposit: When money is deposited the statement is updated

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -35,7 +35,7 @@ describe Account do
   end
 
   # is this test  needed?
-  it 'customers can deposit multiple amountsmoney' do
+  it 'customers can deposit multiple amounts of money' do
     account = Account.new
     john = Customer.new('John Smith', 'password1')
     account.register(john)
@@ -45,6 +45,17 @@ describe Account do
     account.deposit(50)
     expect(account.balance).to eq 200
   end
-
+ 
+  it 'When money is deposited the statement is updated' do
+    #allow(Time).to receive(:now).and_return('2012-01-13 18:31:11.682419000 +0100')
+    @test_time = Time.parse("2012-01-13")
+    allow(Time).to receive(:now).and_return @test_time
+    account = Account.new  
+    john = Customer.new('John Smith', 'password1')
+    account.register(john)
+    account.sign_in('John Smith', 'password1')
+    account.deposit(50)
+    expect(account.statement).to eq([{ credit: 50, balance: 50, date: '13/01/2012' }])
+  end
 
 end
