dbad81208e456cc376639d730694b32b4c312e20
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-18 15:16:34 +0100

    GemFile updated

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -6,8 +6,15 @@ describe Customer do
     customer = Customer.new(name, "password1")
     expect(customer.name).to eq name
   end
-  it 'can set password' do
+  it 'can set an encrypted password' do
     customer = Customer.new(name, 'password1')
-    expect(customer.password).to eq 'password1'
+    expect(customer.password).to_not eq 'password1'
   end
+  it 'can match an encrypted password' do
+    customer = Customer.new(name, 'password1')
+    encrypted_password = Password.new(customer.password)
+    encrypted_password == 'password1'
+  end
+
 end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,20 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+
+
+
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
