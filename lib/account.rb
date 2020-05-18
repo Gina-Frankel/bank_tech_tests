01c3fd851d5fb385cfb18e2f2118b0c84acd60a1
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-18 14:51:00 +0100

    .Account can register a new customer with a password

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,12 @@
 class Account
-  attr_reader :customer_name
+  attr_reader :customer_name, :password
   def initialize
     @customer_name
+    @password
   end
-  def register(customer) 
-    @customer_name = customer.name 
-  end 
+
+  def register(customer)
+    @customer_name = customer.name
+    @password = customer.password
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,19 @@
+# frozen_string_literal: true
+
 require 'account'
 
 describe Account do
   it 'can register a new customer with their name' do
-    account = Account.new 
-    customer = Customer.new("John Smith")
+    account = Account.new
+    customer = Customer.new('John Smith', 'password1')
     account.register(customer)
-    expect(account.customer_name).to eq "John Smith"
+    expect(account.customer_name).to eq 'John Smith'
+  end
+
+  it 'can register a new customer with a password' do
+    account = Account.new
+    customer = Customer.new('John Smith', 'password1')
+    account.register(customer)
+    expect(account.password).to eq customer.password
   end
 end
