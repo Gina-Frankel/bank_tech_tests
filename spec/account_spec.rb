bd8001e1a94d2045076095e77ac8304d99a01831
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-19 10:36:35 +0100

    Reviewed requirements - delete code & tests related to sign_in

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,39 +6,13 @@ describe Account do
   subject(:account) { Account.new }
   let(:john) { Customer.new('John Smith', 'password1') }
 
-  context '#register' do
-    it 'can register a new customer with their name' do
-      customer = Customer.new('John Smith', 'password1')
-      account.register(customer)
-      expect(account.customer_name).to eq 'John Smith'
-    end
-    it 'can register a new customer with a password' do
-      customer = Customer.new('John Smith', 'password1')
-      account.register(customer)
-      expect(account.password).to eq 'password1'
-    end
-  end
 
-  context '#sign_in' do
-    it 'customers can sign in' do
-      john = Customer.new('John Smith', 'password1')
-      account.register(john)
-      account.sign_in('John Smith', 'password1')
-      expect(account.signed_in).to eq true
-    end
 
-    it 'if customers enter wrong password an error is thrown' do
-      john = Customer.new('John Smith', 'right_password')
-      account.register(john)
-      expect { account.sign_in('John Smith', 'wrong_password') }.to raise_error 'Wrong name or password'
-    end
-  end
 
   context '#deposit' do
     it 'customers can deposit money' do
       john = Customer.new('John Smith', 'password1')
       account.register(john)
-      account.sign_in('John Smith', 'password1')
       account.deposit(50)
       expect(account.balance).to eq 50
     end
@@ -47,7 +21,6 @@ describe Account do
     it 'customers can deposit multiple amounts of money' do
       john = Customer.new('John Smith', 'password1')
       account.register(john)
-      account.sign_in('John Smith', 'password1')
       3.times { account.deposit(50) }
       expect(account.balance).to eq 150
     end
@@ -56,8 +29,7 @@ describe Account do
       @test_time = Time.parse('2012-01-13')
       allow(Time).to receive(:now).and_return @test_time
       john = Customer.new('John Smith', 'password1')
-      account.register(john)
-      account.sign_in('John Smith', 'password1')
+      account.register(john) 
       account.deposit(50)
       expect(account.statement).to eq([{ credit: 50, balance: 50, date: '13/01/2012' }])
     end
@@ -67,7 +39,6 @@ describe Account do
       allow(Time).to receive(:now).and_return @test_time
       john = Customer.new('John Smith', 'password1')
       account.register(john)
-      account.sign_in('John Smith', 'password1')
       account.deposit(50)
       account.deposit(100)
       expect(account.statement).to eq([
@@ -75,5 +46,15 @@ describe Account do
                                         { credit: 100, balance: 150, date: '13/01/2012' }
                                       ])
     end
+    # context '#withdraw' do
+    #   it 'When money is withdrawn balance will decrease' do
+    #     john = Customer.new('John Smith', 'password1')
+    #     account.register(john)
+    #     account.sign_in('John Smith', 'password1')
+    #     account.deposit(50)
+    #     account.withdraw(25)
+    #     expect(account.balance).to eq 25
+    #   end 
+    # end
   end
 end
