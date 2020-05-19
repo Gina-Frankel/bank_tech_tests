702ad14eb19081fd69792b38fed265ea50d4248e
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-19 13:26:58 +0100

    Passing .Account #Print_statement: Customer can print statement

--- lib/account.rb
+++ lib/account.rb
@@ -8,19 +8,10 @@ class Account
     @statement = [ ]
   end
 
- 
 
   def deposit(money)
     @balance += money
     record_credit(money)
-  end
-
-  def date
-    Time.now.strftime('%d/%m/%Y')
-  end
-
-  def record_credit(money)
-    @statement.push({ credit: money, balance: @balance, date: date })
   end
 
   def withdraw(money)
@@ -28,8 +19,26 @@ class Account
     record_debit(money)
   end 
 
+  def print_statement
+    puts "date || credit || debit || balance"
+    @statement.reverse!
+    @statement.each do |transaction|
+      #puts transaction
+      puts "#{transaction[:date]} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]}"
+    end 
+  end 
+
+  def record_credit(money)
+    @statement.push({ credit: money, balance: @balance, date: date })
+  end
+
   def record_debit(money)
     @statement.push({ debit: money, balance: @balance, date: date })
   end
+
+  def date
+    Time.now.strftime('%d/%m/%Y')
+  end
+
 
 end
