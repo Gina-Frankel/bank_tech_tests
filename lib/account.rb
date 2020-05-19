70381f780b1266c2835a3e6d214ede6eb93be492
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-19 10:36:35 +0100

    Reviewed requirements - delete code & tests related to sign_in

--- lib/account.rb
+++ lib/account.rb
@@ -13,18 +13,6 @@ class Account
     @password = customer.password
   end
 
-  def sign_in(name, password)
-    if validate?(name, password) 
-      @signed_in = true
-    else 
-      raise 'Wrong name or password'
-    end 
-  end
-
-  def validate?(name, password)
-    name == @customer_name && @password == password
-  end
-
   def deposit(money)
     @balance += money
     record_credit(money)
