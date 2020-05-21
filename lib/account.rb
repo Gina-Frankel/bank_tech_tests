2d6da48c1e9b7246ad461c8474e27f68ba9b8851
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-21 14:17:41 +0100

    Refractor .Account - Delete methods that are implemented by .TransactionRecorder class

--- lib/account.rb
+++ lib/account.rb
@@ -13,33 +13,23 @@ class Account
     statement.unshift(transaction_recorder_creator.record(credit:money))
   end
   
-
-def transaction_recorder_creator
-  TransactionRecorder.new(balance: balance)
-end 
-
   def withdraw(money)
     raise 'You have no money in your account' if balance == 0 
     minus_withdrawal(money)
     statement.unshift(transaction_recorder_creator.record(debit:money))
   end
 
-
   def send_printer
     printer = Printer.new
     printer.print_statement(statement)
   end
 
-
-  def record_debit(money)
-    statement.unshift({ credit: nil, debit: money, balance: balance, date: date })
-  end
-
-  def date
-    Time.now.strftime('%d/%m/%Y')
-  end
-
   private 
+
+  def transaction_recorder_creator
+    TransactionRecorder.new(balance: balance)
+  end 
+
   def add_deposit(money)
     @balance += money
   end 
