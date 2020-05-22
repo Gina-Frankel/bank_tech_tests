f3d943b5b2ed7a45e668a2270d6ed06386fb0e4b
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-22 12:45:07 +0100

    Lint & refractor tests for clarity

--- spec/features/user_can_print_statement_spec.rb
+++ spec/features/user_can_print_statement_spec.rb
@@ -1,21 +1,19 @@
-require'account'
+require 'account'
 
 context '#User prints statement of transactions' do
   it 'Customer can print statement' do
-    # Set  up
+
     test_time = Time.parse('2012-01-13')
     allow(Time).to receive(:now).and_return test_time
 
-  
-    # exersise - the behaviour of the user in IRB
     account = Account.new
     account.deposit(50)
     account.withdraw(30)
 
-      # To make the expect statement more readable
-      printed_header = "date || credit || debit || balance\n"
-      newest_transaction = "13/01/2012 ||  || 30.00 || 20.00\n"
-      oldest_transaction = "13/01/2012 || 50.00 ||  || 50.00\n"
-    expect { account.print_statement }. to output(printed_header + newest_transaction + oldest_transaction).to_stdout
+    printed_header = "date || credit || debit || balance\n"
+    newest_transaction = "13/01/2012 ||  || 30.00 || 20.00\n"
+    oldest_transaction = "13/01/2012 || 50.00 ||  || 50.00\n"
+    statement = printed_header + newest_transaction + oldest_transaction
+    expect { account.print_statement }. to output(statement).to_stdout
   end
 end
