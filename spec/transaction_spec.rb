7c89870e5c8593e421f3a8dc20a1ff70e4bc3f88
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-22 14:46:54 +0100

    Add test to test for atribute -credit

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,8 +1,15 @@
 require 'transaction'
 
 describe Transaction do
-  it 'when transaction is initialized it has attributes of date'do
-    transaction = Transaction.new(credit:nil, debit: nil)
-    expect(transaction).to have_attributes(:balance => 0)
+  it 'when transaction is initialized with a default balance of 0'do
+    transaction = Transaction.new(credit: nil, debit: nil)
+    expect(transaction).to have_attributes(balance: 0)
   end 
+
+  it 'when transaction can be initialized with credit value'do
+    transaction = Transaction.new(credit: 50, debit: nil)
+    expect(transaction).to have_attributes(credit: 50)
+  end 
+
+
 end
