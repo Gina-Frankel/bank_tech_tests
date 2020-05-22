954aac5f6d9f1c64a1e096c87d7342cdb9f4d6b9
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-22 14:44:59 +0100

    Add test to test for atributes

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,17 +1,8 @@
 require 'transaction'
 
 describe Transaction do
-  it 'can format time' do
-    transaction = Transaction.new(balance: 0, credit: nil, debit: nil)
-    test_time = Time.parse('2020-05-21 11:55:00 +0100 ')
-    allow(Time).to receive(:now).and_return test_time
-
-    expect(transaction.format_day_month_year(test_time)).to eq '21/05/2020'
-  end
-
-  it 'can add pence' do
-    transaction = Transaction.new(balance: 0, credit: nil, debit: nil)
-
-    expect(transaction.add_pence(50)).to eq '50.00'
-  end
+  it 'when transaction is initialized it has attributes of date'do
+    transaction = Transaction.new(credit:nil, debit: nil)
+    expect(transaction).to have_attributes(:balance => 0)
+  end 
 end
