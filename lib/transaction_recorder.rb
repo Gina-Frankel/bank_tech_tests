3252d83f5d8cc74506de5cad1d7bfd848e5366fa
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-22 11:36:06 +0100

    passing - changed test .Printer to include instance double

--- lib/transaction_recorder.rb
+++ lib/transaction_recorder.rb
@@ -7,10 +7,18 @@ class Transaction
     @debit = add_pence(debit) 
     @balance = add_pence(balance)
     @date  = format_day_month_year(Time.now)
+
    #@transaction = { credit: add_pence(credit), debit: add_pence(debit), balance: add_pence(balance), date: format_day_month_year }
   end
 
   private
+
+# attributes have too many methods ? maybe display method and then send it back or move this to another class   
+def display
+  
+end 
+
+
 
   def format_day_month_year(time)
     time.strftime('%d/%m/%Y')
--- spec/transaction_recorder_spec.rb
+++ spec/transaction_recorder_spec.rb
@@ -3,6 +3,7 @@ require 'transaction_recorder'
 describe Transaction  do
 
 
+
   it  'can format time' do
     transaction = Transaction.new 
     test_time = Time.parse('2020-05-21 11:55:00 +0100 ')
@@ -13,5 +14,39 @@ describe Transaction  do
 
 
 
-  
+
+  #process notes - this test should have been completed first
+  it 'records default balance from initialisation as 0.00' do
+    transaction = Transaction.new
+    expect(transaction.record).to eq({ credit: nil, debit: nil, balance: '0.00', date: test_time })
+  end
+
+  it 'records the date when recording a transaction' do
+    transaction_recorder = Transaction.new 
+
+    test_time = Time.parse('2020-05-21 11:55:00 +0100 ')
+    allow(Time).to receive(:now).and_return test_time
+    
+    # might caues other tests to break when connect .Account class
+    expect(transaction_recorder.record).to eq({ credit: nil, debit: nil, balance: '0.00', date: '21/05/2020' })
+  end 
+
+  it 'records credit when recording a transaction with updated balance' do
+    transaction_recorder = Transaction.new(balance: 500)
+
+    expect(transaction_recorder.record(credit: 500)).to eq({ credit: '500.00', debit: nil, balance: '500.00', date: test_time })
+  end
+
+  it 'records debit when recording a transaction with updated balance' do
+    transaction_recorder = Transaction.new(balance: 300)
+
+    expect(transaction_recorder.record(debit: 20)).to eq({ credit: nil, debit: '20.00', balance: '300.00', date: test_time })
+  end
+ 
+
+end
+
+def test_time
+  Time.now.strftime('%d/%m/%Y')
+end 
 
