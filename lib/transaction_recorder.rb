6d52f0ba7085a550d796697dc57a8f33ab52669f
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-22 11:54:41 +0100

    Unit Test - Passing: 'can format time'

--- lib/transaction_recorder.rb
+++ lib/transaction_recorder.rb
@@ -6,12 +6,12 @@ class Transaction
     @credit = add_pence(credit)
     @debit = add_pence(debit) 
     @balance = add_pence(balance)
-    @date  = format_day_month_year(Time.now)
+    @date  = Time.now
 
    #@transaction = { credit: add_pence(credit), debit: add_pence(debit), balance: add_pence(balance), date: format_day_month_year }
   end
 
-  private
+  
 
 # attributes have too many methods ? maybe display method and then send it back or move this to another class   
 def display
--- spec/transaction_recorder_spec.rb
+++ spec/transaction_recorder_spec.rb
@@ -5,11 +5,11 @@ describe Transaction  do
 
 
   it  'can format time' do
-    transaction = Transaction.new 
+    transaction = Transaction.new(balance: 0, credit: nil , debit: nil) 
     test_time = Time.parse('2020-05-21 11:55:00 +0100 ')
     allow(Time).to receive(:now).and_return test_time
 
-    expect(transaction.format_day_month_year(test_time)).to eq '21/05/21'
+    expect(transaction.format_day_month_year(test_time)).to eq '21/05/2020'
   end
 
 end 
