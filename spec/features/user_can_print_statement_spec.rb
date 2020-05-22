a57db8dfcc9b52ebf9570d480b2ffdcb3d8082f2
‘Gina-Frankel <g.frankel22@gmail.com>
2020-05-22 13:20:55 +0100

    Tests using requirements

--- spec/features/user_can_print_statement_spec.rb
+++ spec/features/user_can_print_statement_spec.rb
@@ -3,17 +3,24 @@ require 'account'
 context '#User prints statement of transactions' do
   it 'Customer can print statement' do
 
-    test_time = Time.parse('2012-01-13')
-    allow(Time).to receive(:now).and_return test_time
+    newest_time = Time.parse('2020-05-21 11:55:00 +0100 ')
+    allow(Time).to receive(:now).and_return newest_time
+
+ 
+
 
     account = Account.new
-    account.deposit(50)
-    account.withdraw(30)
+    account.deposit(1000)
+    account.deposit(2000)
+    account.withdraw(500)
+
+
 
     printed_header = "date || credit || debit || balance\n"
-    newest_transaction = "13/01/2012 ||  || 30.00 || 20.00\n"
-    oldest_transaction = "13/01/2012 || 50.00 ||  || 50.00\n"
-    statement = printed_header + newest_transaction + oldest_transaction
+    newest_transaction = "21/05/2020 ||  || 500.00 || 2500.00\n"
+    middle_transaction = "21/05/2020 || 2000.00 ||  || 3000.00\n"
+    oldest_transaction = "21/05/2020 || 1000.00 ||  || 1000.00\n"
+    statement = printed_header + newest_transaction + middle_transaction + oldest_transaction
     expect { account.print_statement }. to output(statement).to_stdout
   end
 end
