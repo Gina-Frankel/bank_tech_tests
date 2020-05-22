417098a9c6ad03a24a025e3930282b7edfb40c65
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-22 16:08:09 +0100

    Add test - transaction  initialized with time value

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,11 +1,11 @@
 class Transaction
 
-  attr_reader :credit, :balance, :debit, :date
-  def initialize(balance: 0, credit: nil , debit: nil)
+  attr_reader :credit, :balance, :debit, :time 
+  def initialize(balance: 0, credit: nil , debit: nil )
     @credit = credit
     @debit = debit
     @balance = balance
-    @date  = format_day_month_year(Time.now)
+    @time  = Time.now
   end
 
   def format_day_month_year(time)
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,20 +1,31 @@
 require 'transaction'
 
 describe Transaction do
-  it 'when transaction is initialized with a default balance of 0'do
-    transaction = Transaction.new(credit: nil, debit: nil)
+  
+  it 'when transaction is initialized with a default balance of 0' do
+    transaction = Transaction.new()
     expect(transaction).to have_attributes(balance: 0)
-  end 
+  end
 
-  it 'when transaction can be initialized with credit value'do
+  it 'transaction can be initialized with given credit value' do
     transaction = Transaction.new(credit: 50)
     expect(transaction).to have_attributes(credit: 50)
-  end 
+  end
 
-  it 'when transaction can be initialized with debit value'do
-  transaction = Transaction.new(debit: 100)
-  expect(transaction).to have_attributes(debit: 100)
-end 
+  it 'transaction can be initialized with given debit value' do
+    transaction = Transaction.new(debit: 100)
+    expect(transaction).to have_attributes(debit: 100)
+  end
 
+  it ' transaction will be initialized with time value' do
+    test_time = Time.now
+    allow(Time).to receive(:now).and_return(test_time)
+    transaction = Transaction.new()
+   
+
+    expect(transaction).to have_attributes(time: test_time)
+  end
 
 end
+
+# could also test that class can know  it's attributes 
